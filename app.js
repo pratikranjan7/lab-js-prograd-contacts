27257a29e244312635c37c9dad79817622b7aa87
Pratik Ranjan <pratikranjan4597@gmail.com>
2022-07-21 00:39:34 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//Enter your code here..
+async function contacts() {
+  try {
+    let res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    let result = await res.json();
+    console.log(result);
+
+    var player = "<h2>Lists of Users</h2>";
+    result.forEach(function (user) {
+      let para = document.createElement("p");
+      para.className = "newbox";
+      player += `<div class="player">
+                        <div class="strength">Name : ${user.name}</div>
+                        <div>Email   : ${user.email}</div>
+                        <div>Phone   : ${user.phone}</div>
+                        <div>Website : ${user.website}</div>
+                        <div>Company : ${user.company.name}</div>
+                        <div>City    : ${user.address.city}</div>
+                        <div>Zipcode : ${user.address.zipcode}</div>
+                        </div>`;
+    });
+
+    document.getElementById("message").innerHTML = player;
+  } catch (error) {}
+}
